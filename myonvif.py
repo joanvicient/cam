bfe6c5de6c031199139b1d7fabd79c174e1ad2e1
Joan Vicient <lo.roquetes@gmail.com>
2025-02-23 11:52:16 +0100

    - Refactored as a class - Added pep8 beautifier in code workspace

--- myonvif.py
+++ myonvif.py
@@ -9,92 +9,114 @@ import os
 
 WSDL_PATH = os.path.join(os.path.dirname(__file__), 'python-onvif-zeep/wsdl')
 
-def discover_onvif_cameras():
-    try:
-        from wsdiscovery import WSDiscovery
-        wsd = WSDiscovery()
-        wsd.start()
-        services = wsd.searchServices()
-        wsd.stop()
-        
-        cameras = []
-        for service in services:
-            url = service.getXAddrs()[0]
-            if 'onvif' in url:
-                parsed_url = urlparse(url)
-                cam = {}
-                cam['ip'] = parsed_url.hostname
-                cam['port'] = parsed_url.port if parsed_url.port else 80
-                cameras.append(cam)
 
-        if cameras:
-            print("Discovered ONVIF Cameras:")
-            for cam in cameras:
-                print('- ' + str(cam))
+class myOnvifClass:
+
+    def __init__(self, wsdl_path='/etc/onvif/wsdl'):
+        try:
+            from wsdiscovery import WSDiscovery
+            wsd = WSDiscovery()
+            wsd.start()
+            services = wsd.searchServices()
+            wsd.stop()
+
+            self.cameras = {}
+            for service in services:
+                url = service.getXAddrs()[0]
+                if 'onvif' in url:
+                    parsed_url = urlparse(url)
+                    ip = parsed_url.hostname
+                    port = parsed_url.port if parsed_url.port else 80
+                    camera = self.identify_camera(ip, port, wsdl_path)
+                    if camera:
+                        self.cameras[camera['name']] = camera
+
+            if self.cameras:
+                print("Discovered ONVIF Cameras:")
+                for camera in self.cameras:
+                    print('- ' + camera)
+            else:
+                print("No ONVIF cameras found.")
+
+        except Exception as e:
+            print("Error discovering cameras:", e)
+
+    def identify_camera(self, ip, port, wsdl_path):
+        camera = {}
+        camera['ip'] = ip
+        camera['port'] = port
+        camera['username'] = 'admin'
+        camera['password'] = 'Joan35Joan'
+        try:
+            onvif_camera = ONVIFCamera(
+                camera['ip'], camera['port'], camera['username'], camera['password'], wsdl_path)
+            media_service = onvif_camera.create_media_service()
+
+            # Create a device management service
+            device_mgmt = onvif_camera.create_devicemgmt_service()
+
+            # Get basic device information
+            device_info = device_mgmt.GetDeviceInformation()
+            camera['manufacturer'] = device_info.Manufacturer
+            camera['model'] = device_info.Model
+            camera['firmware_version'] = device_info.FirmwareVersion
+            camera['serial_number'] = device_info.SerialNumber
+            camera['hardware_id'] = device_info.HardwareId
+            if device_info.Manufacturer == 'AQE':
+                camera['name'] = "tortuges"
+            elif device_info.Manufacturer == 'LC':
+                camera['name'] = "terrassa"
+            else:
+                camera['name'] = "unknown"
+
+            # Get profiles
+            profiles = media_service.GetProfiles()
+            profile_token = profiles[0].token  # Use the first profile
+
+            # Get the snapshot URI
+            request = media_service.create_type('GetSnapshotUri')
+            request.ProfileToken = profile_token
+            camera['uri'] = media_service.GetSnapshotUri(request).Uri
+
+            # Save changes
+            return camera
+
+        except Exception as e:
+            print("Error connecting to camera:", e)
+            return []
+
+    def get_snapshot(self, name):
+        camera = self.cameras[name]
+        print("Retrieving snapshot from", name)
+        response = requests.get(camera['uri'], auth=HTTPDigestAuth(
+            camera['username'], camera['password']), stream=True)
+        if response.status_code == 200:
+            image = Image.open(BytesIO(response.content))
+            return image
         else:
-            print("No ONVIF cameras found.")
-        return cameras
-    except Exception as e:
-        print("Error discovering cameras:", e)
-        return []
+            print("Failed to retrieve snapshot. Status code:",
+                  response.status_code)
+            return None
 
-def identify_camera(cam_dict, wsdl_path='/etc/onvif/wsdl'):                
-    cam_dict['username'] = 'admin'
-    cam_dict['password'] = 'Joan35Joan'
-    try:
-        onvif_camera = ONVIFCamera(cam_dict['ip'], cam_dict['port'], cam_dict['username'], cam_dict['password'], wsdl_path)
-        media_service = onvif_camera.create_media_service()
+    def show_snapshot(self, camera_name):
+        image = self.get_snapshot(camera_name)
+        if image:
+            image.show()
 
-        # Create a device management service
-        device_mgmt = onvif_camera.create_devicemgmt_service()
+    def save_snapshot(self, camera_name, image_name):
+        image = self.get_snapshot(camera_name)
+        if image:
+            image.save(image_name)
+            print("Snapshot saved as 'snapshot.jpg'")
 
-        # Get basic device information
-        device_info = device_mgmt.GetDeviceInformation()
-        cam_dict['manufacturer'] = device_info.Manufacturer
-        cam_dict['model'] = device_info.Model
-        cam_dict['firmware_version'] = device_info.FirmwareVersion
-        cam_dict['serial_number'] = device_info.SerialNumber
-        cam_dict['hardware_id'] = device_info.HardwareId
-        if device_info.Manufacturer == 'AQE':
-            cam_dict['name'] = "tortuges"
-        elif device_info.Manufacturer == 'LC':
-            cam_dict['name'] = "terrassa"
-        else:
-            cam_dict['name'] = "unknown"
-        
-        # Get profiles
-        profiles = media_service.GetProfiles()
-        profile_token = profiles[0].token  # Use the first profile
-        
-        # Get the snapshot URI
-        request = media_service.create_type('GetSnapshotUri')
-        request.ProfileToken = profile_token
-        cam_dict['uri'] = media_service.GetSnapshotUri(request).Uri
+    def get_camera_list(self):
+        return self.cameras.keys()
 
-    except Exception as e:
-        print("Error connecting to camera:", e)
-        return None
-
-    return cam_dict
-
-def get_snapshot(cam):
-    print("Retrieving snapshot from", cam['name'])
-    response = requests.get(cam['uri'], auth=HTTPDigestAuth(cam['username'], cam['password']), stream=True)
-    if response.status_code == 200:
-        image = Image.open(BytesIO(response.content))
-        image.show()  # Display image
-        #image.save("snapshot.jpg")  # Save image
-        #print("Snapshot saved as 'snapshot.jpg'")
-    else:
-        print("Failed to retrieve snapshot. Status code:", response.status_code)
 
 if __name__ == "__main__":
     # Discover cameras
-    cameras = discover_onvif_cameras()
+    cameras = myOnvifClass(wsdl_path=WSDL_PATH)
 
-    for camera in cameras:
-        print("Connecting to", camera["ip"])
-
-        camera = identify_camera(camera, wsdl_path=WSDL_PATH)
-        if camera:
-            get_snapshot(camera)
+    for camera in cameras.get_camera_list():
+        cameras.show_snapshot(camera)
+        # cameras.save_snapshot(camera, camera+".jpg")
